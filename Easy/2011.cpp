7578b72dd2ee5a9d6578feb3258cbb9b20567889
Ruturaj Panditrao <94856890+Ruturaj-Panditrao@users.noreply.github.com>
2023-12-30 21:35:28 +0530

    Create 2011.cpp

---
+++ Easy/2011.cpp
@@ -0,0 +1,20 @@
+// Final Value of Variable After Performing Operations
+// https://leetcode.com/problems/final-value-of-variable-after-performing-operations/
+
+class Solution 
+{
+public:
+    int finalValueAfterOperations(vector<string>& operations)
+    {
+        int ans = 0;
+        for(int i=0; i<operations.size(); i++)
+        {
+            if(operations[i]=="X++" || operations[i]=="++X") ans++;
+            else ans--;
+        }
+        return ans;
+    }
+};
+
+// Time Complexity : O(len(operations))
+// Space Complexity : O(1) 
