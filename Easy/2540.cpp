94fb43318c2db5bfa5fd667e3bbd637ecd9821c2
Ruturaj Panditrao <94856890+Ruturaj-Panditrao@users.noreply.github.com>
2024-03-09 23:50:18 +0530

    Create 2540.cpp

---
+++ Easy/2540.cpp
@@ -0,0 +1,14 @@
+// https://leetcode.com/problems/minimum-common-value/description/
+
+class Solution {
+public:
+   int getCommon(vector<int>& nums1, vector<int>& nums2) {
+    int i = 0, j = 0;
+    while(i < nums1.size() && j < nums2.size() ){
+        if(nums1[i] == nums2[j]) return nums1[i];
+        if(nums1[i] > nums2[j]) j++;
+        else i++;
+    }
+    return -1;
+}
+};
