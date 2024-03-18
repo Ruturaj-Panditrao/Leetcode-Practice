672adf5aef8640aaca5082f4c83d73b570531705
Ruturaj Panditrao <94856890+Ruturaj-Panditrao@users.noreply.github.com>
2024-03-18 23:48:15 +0530

    Create 452.cpp

---
+++ Medium/452.cpp
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/minimum-number-of-arrows-to-burst-balloons/
+
+class Solution 
+{
+public:
+    int findMinArrowShots(vector<vector<int>>& points) 
+    {
+        sort(points.begin(), points.end());
+        int ans = 1, last = points[0][1], n = points.size();
+
+        for(int i=1; i<n; i++) {
+            if(last < points[i][0]) ans++, last = points[i][1];
+            else last = min(last, points[i][1]);
+        }
+        return ans;
+    }
+};
